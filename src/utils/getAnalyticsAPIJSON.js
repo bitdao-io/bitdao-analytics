72fb59f9c4b614b9f40d104522eec0bad0c6e843
Tyler Smith <mail@tcry.pt>
2021-10-20 05:15:13 -0700

    TWEAK: Add usdTotal.

--- src/utils/getAnalyticsAPIJSON.js
+++ src/utils/getAnalyticsAPIJSON.js
@@ -6,12 +6,18 @@ async function getAnalyticsAPIJSON(conf, conns){
     let balances = await getBalances(conns, conf.treasuryAddress);
     let prices = await getPrices();
 
-    return tokens.reduce((resp, token) => {
+    let usdTotal = 0;
+    let response = tokens.reduce((resp, token) => {
         let symbol = token.symbol.toLocaleLowerCase();
         resp[symbol + "Count"] = balances[token.symbol];
         resp[symbol + "Price"] = prices[token.symbol];
+        usdTotal += balances[token.symbol] * prices[token.symbol];
         return resp;
     }, {});
+
+    response.usdTotal = usdTotal;
+
+    return response;
 }
 
 module.exports = getAnalyticsAPIJSON;

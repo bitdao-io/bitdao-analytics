4908ff49c6a6c2eae0fcdcd00646d3650eb490ca
Tyler Smith <mail@tcry.pt>
2021-10-23 21:43:06 -0700

    TWEAK: Add FTT data.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,6 +8,10 @@ module.exports = {
             address: '0x1a4b46696b2bb4794eb3d4c26f1c55f9170fa4c5',
         },
         {
+            symbol: 'FTT',
+            address: '0x50d1c9771902476076ecfc8b2a83ad6b9355a4c9',
+        },
+        {
             symbol: 'USDT',
             address: '0xdAC17F958D2ee523a2206206994597C13D831ec7'
         },

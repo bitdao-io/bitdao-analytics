ed8578291ba6accdb4ca9098084dc11d7625430c
Tyler Smith <mail@tcry.pt>
2021-12-15 09:42:46 -0800

    BUGFIX: Fix stray text.

--- src/api/getChart.ts
+++ src/api/getChart.ts
@@ -2,10 +2,9 @@ import {ChartBody} from "../models";
 import {
     getContributionsForHistoricalVolumes
 } from "../getters/getExpectedContribution";
-a
-exqport default async function getChart(config: any, conns: any): Promise<ChartBody> {
+
+export default async function getChart(config: any, conns: any): Promise<ChartBody> {
     let chartBody = new ChartBody();
     chartBody.list = await getContributionsForHistoricalVolumes();
     return chartBody;
 };
-qq

7a4025e20805351db32cbd8b1d61444f273c32ca
Tyler Smith <mail@tcry.pt>
2022-08-16 11:19:42 -0400

    BUGFIX,CLEANUP: Fix merge error and make it easier to read.

--- src/lambdas/updateAPIS3.ts
+++ src/lambdas/updateAPIS3.ts
@@ -1,43 +1,44 @@
-import newConfigFromEnv from '../config';
-import newConnections from '../connections';
-import {uploadFile} from '../s3';
-import getBalances from '../getters/getBalances';
-import getContributions from "../getters/getContributions";
+import newConfigFromEnv from '../config'
+import newConnections from '../connections'
+import {uploadFile} from '../s3'
+import getBalances from '../getters/getBalances'
+import getContributions from '../getters/getContributions'
 
-const config = newConfigFromEnv();
-const {s3, web3} = newConnections(config);
+const config = newConfigFromEnv()
+const {
+    s3,
+    web3
+} = newConnections(config)
 
 function formatDateForName(date: Date) {
-    const pad = (n: number) => (n < 10 ? '0' : '') + n;
-    const y = date.getUTCFullYear();
-    const m = pad(date.getUTCMonth() + 1);
-    const d = pad(date.getUTCDate());
-    return y + '-' + m + '-' + d;
+    const pad = (n: number) => (n < 10 ? '0' : '') + n
+    const y = date.getUTCFullYear()
+    const m = pad(date.getUTCMonth() + 1)
+    const d = pad(date.getUTCDate())
+    return y + '-' + m + '-' + d
 }
 
 
-function writeToS3(name: string, func: Function) : Promise<string> {
-    return func().then((resp: any) => {
-        const json = JSON.stringify({success: true, body: resp});
-        uploadFile(s3, config.s3.bucket, 'analytics/'+name+'.json', json);
-        uploadFile(s3, config.s3.bucket, 'analytics/'+name+'-' + formatDateForName(new Date()) + '.json', json);
-        return true;
-    });
+function writeToS3(name: string, body: any): Promise<[string, string]> {
+    const json = JSON.stringify({
+        success: true,
+        body: body
+    })
+    return Promise.all([
+      uploadFile(s3, config.s3.bucket, 'analytics/' + name + '.json', json),
+      uploadFile(s3, config.s3.bucket, 'analytics/' + name + '-' + formatDateForName(new Date()) + '.json', json)
+    ])
 }
 
-function print(func: Function) : Promise<string> {
+function print(func: Function): Promise<string> {
     return func().then((resp: any) => {
-        console.log("---");
-        console.log(JSON.stringify(resp));
-        return true;
-    });
+        console.log('---')
+        console.log(JSON.stringify(resp))
+        return true
+    })
 }
 
 export default async function handler() {
-    return Promise.all([
-        writeToS3('balance', () => {
-            return getBalances(web3, config.treasuryAddress);
-        }),
-        print(getContributions),
-    ]);
+    await writeToS3('balance', await getBalances(web3, config.treasuryAddress)),
+    await writeToS3('chart-100', await getContributions())
 }
